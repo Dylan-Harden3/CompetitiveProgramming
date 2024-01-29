1ac3f4a4b90e6f81bf2d58b220d1dbcb1dfe7ab1
dylanharden3 <dylanharden3@tamu.edu>
2024-01-29 10:52:12 -0600

    Update LC 232

--- LeetCode/232_ImplementQueueUsingStacks.py
+++ LeetCode/232_ImplementQueueUsingStacks.py
@@ -1,25 +1,33 @@
 class MyQueue:
 
     def __init__(self):
-        self.stack = collections.deque()
+        self.stack1 = collections.deque()
+        self.stack2 = collections.deque()
 
     def push(self, x: int) -> None:
-        self.stack.appendleft(x)
+        self.stack1.append(x)
 
     def pop(self) -> int:
-        for i in range(len(self.stack)-1):
-            self.stack.append(self.stack.popleft())
-        return self.stack.popleft()
+        for i in range(len(self.stack1)-1):
+            self.stack2.append(self.stack1.pop())
+        res = self.stack1.pop()
+
+        while len(self.stack2):
+            self.stack1.append(self.stack2.pop())
+        return res
+
 
     def peek(self) -> int:
-        for i in range(len(self.stack)-1):
-            self.stack.append(self.stack.popleft())
-        res = self.stack[0]
-        self.stack.append(self.stack.popleft())
+        for i in range(len(self.stack1)-1):
+            self.stack2.append(self.stack1.pop())
+        res = self.stack1[-1]
+
+        while len(self.stack2):
+            self.stack1.append(self.stack2.pop())
         return res
 
     def empty(self) -> bool:
-        return len(self.stack) == 0
+        return len(self.stack1) == 0
 
 
 # Your MyQueue object will be instantiated and called as such:
