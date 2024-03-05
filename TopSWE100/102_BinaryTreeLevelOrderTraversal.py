50b58acabe77844c759eea6a5e0be644865805ab
Dylan-Harden3 <dylanharden33@gmail.com>
2024-03-05 17:25:03 -0600

    TopSWE100 25/100

---
+++ TopSWE100/102_BinaryTreeLevelOrderTraversal.py
@@ -0,0 +1,20 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def levelOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
+        levels = defaultdict(list)
+
+        def helper(node, level):
+            if not node: return
+
+            levels[level].append(node.val)
+            helper(node.left, level+1)
+            helper(node.right, level+1)
+
+        helper(root, 0)
+
+        return levels.values()
