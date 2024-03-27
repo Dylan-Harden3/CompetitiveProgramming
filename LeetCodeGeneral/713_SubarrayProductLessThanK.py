e915cc2cd55d89ccca98221206b70ab6c68189ef
Dylan-Harden3 <dylanharden33@gmail.com>
2024-03-27 10:45:15 -0500

    LC Daily

---
+++ LeetCodeGeneral/713_SubarrayProductLessThanK.py
@@ -0,0 +1,11 @@
+class Solution:
+    def numSubarrayProductLessThanK(self, nums: List[int], k: int) -> int:
+        l = res = 0
+        p = 1
+        for r in range(len(nums)):
+            p *= nums[r]
+            while l <= r and p >= k:
+                p = p // nums[l]
+                l += 1
+            res += (r-l+1)
+        return res
